026514134794a3ebd0898b85a09b9879f677b89a
Marvvo <none@none>
2024-07-16 08:33:41 +0200

    Datei Hinzufügen

---
+++ blink.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+import time
+
+# GPIO-Modus (BOARD / BCM)
+GPIO.setmode(GPIO.BCM)
+
+# GPIO-Pin festlegen
+LED_PIN = 18
+
+# Pin als Ausgang festlegen
+GPIO.setup(LED_PIN, GPIO.OUT)
+
+try:
+    while True:
+        GPIO.output(LED_PIN, GPIO.HIGH)  # LED an
+        time.sleep(1)  # 1 Sekunde warten
+        GPIO.output(LED_PIN, GPIO.LOW)   # LED aus
+        time.sleep(1)  # 1 Sekunde warten
+except KeyboardInterrupt:
+    print("Beenden...")
